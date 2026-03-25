289ba78fd914479ed8a9ac5077c95ed9ad476923
Nolin Borrero <nolin.borrero@live.com>
2026-03-24 22:05:21 -0700

    more cleanup

--- src/core/Renderer.h
+++ src/core/Renderer.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#include "PassType.h"
 #include "PassContext.h"
+#include "PassType.h"
 #include "VulkanContext.h"
 #include "vk_mem_alloc.h"
 #include <GLFW/glfw3.h>
 
 namespace core {
 struct Renderer {
-    VulkanContext vk{};
     VmaAllocator allocator{};
+    core::VulkanContext vk{};
+    core::PassContext pass{};
     struct Window {
         GLFWwindow *glfw_handle{nullptr};
         int32_t width{};
         int32_t height{};
     } window;
-    core::PassContext pass{};
 };
 }; // namespace core
